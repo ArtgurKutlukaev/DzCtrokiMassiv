b396d18b045de01bb91899a886a342faa5e6fcff
ArturKutlukaev <kutlukayev@bk.ru>
2024-05-01 22:01:37 +0500

    first commit

--- src/main/java/ru/netoljogy/javaqa/javaqamvn/services/StatsService.java
+++ src/main/java/ru/netoljogy/javaqa/javaqamvn/services/StatsService.java
@@ -40,22 +40,23 @@ public class StatsService {
 
     public int montSalesAreBelowAverage(long[] sales) {
         int counter = 0;
+        long averageSale = average(sales);
         for (long sale : sales) {
-            if (sale < average(sales)) {
+            if (sale < averageSale) {
                 counter++;
             }
         }
-        return counter++;
+        return counter;
     }
 
     public int montSalesAreAboveAverage(long[] sales) {
         int counter = 0;
         long averageSale = average(sales);
         for (long sale : sales) {
-            if (sale > average(sales)) {
+            if (sale > averageSale) {
                 counter++;
             }
         }
-        return counter++;
+        return counter;
     }
 }
